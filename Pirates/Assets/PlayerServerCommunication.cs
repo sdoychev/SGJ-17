3a24bbb186a6388c9d73934cd3cccd74a714cd25
Stefan Doychev <sdoychev@gmail.com>
2017-01-21 00:15:32 +0200

    basic server implementation

---
+++ Pirates/Assets/PlayerServerCommunication.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerServerCommunication : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public class Puzzle : MessageBase
+    {
+        public int puzzleInt;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            //SolvePuzzle();
+        }
+	}
+
+    
+
+
+}
