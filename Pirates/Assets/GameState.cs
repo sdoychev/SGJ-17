3ff6d012b69514111fea7c4c15569065691dfde4
Kaloyan Kaludov <kaloyan.kaludov@gmail.com>
2017-01-21 18:54:56 +0200

    * Add GameState class to handle said changes

---
+++ Pirates/Assets/GameState.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public enum State
+    {
+        WaitForConnections,
+        WaitForStart,
+        GameRunning,
+        Win,
+        Lose
+    }
+
+    private State oldState;
+    public State state;
+
+	void Start()
+    {
+        oldState = State.WaitForConnections;
+        state = State.WaitForConnections;
+	}
+
+    public void GoToState(State _state)
+    {
+        state = _state;
+    }
+	
+	void Update()
+    {
+        if( state != oldState )
+        {
+            switch( state )
+            {
+            case State.WaitForConnections:
+                {
+                }
+                break;
+
+            case State.WaitForStart:
+                {
+                }
+                break;
+
+            case State.GameRunning:
+                {
+                }
+                break;
+
+            case State.Win:
+                {
+                }
+                break;
+
+            case State.Lose:
+                {
+                }
+                break;
+            }
+
+            oldState = state;
+        }
+	}
+}
