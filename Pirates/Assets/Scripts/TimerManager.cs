288bfb72b00a750c8250c29563de342caa091897
Stefan Doychev <sdoychev@gmail.com>
2017-01-21 13:29:10 +0200

    Start implementing Timer Manager

---
+++ Pirates/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour {
+
+    [SerializeField]
+    private float timeLeft;
+
+    [SerializeField]
+    private GameObject cowsBullsManagerObject;
+
+    [SerializeField]
+    private Image waveEffectImage;
+
+    private CowsBullsManager cowsBullsManager;
+
+    void Start()
+    {
+        cowsBullsManager = cowsBullsManagerObject.GetComponent<CowsBullsManager>();
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        UpdateWaveLevel();
+        if (timeLeft < 0)
+        {
+            print("GAME OVER"); //TODO
+        }
+    }
+
+    public void ReduceTimerTime()
+    {
+        //TODO
+        UpdateWaveLevel();
+    }
+
+    private void UpdateWaveLevel()
+    {
+        //TODO
+    }
+}
