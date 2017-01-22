3ca36d3cef119548690727cf18d2a5603072d3c5
Stefan Doychev <sdoychev@gmail.com>
2017-01-22 06:27:49 +0200

    minor bugfixes

--- Pirates/Assets/Scripts/CowsBullsManager.cs
+++ Pirates/Assets/Scripts/CowsBullsManager.cs
@@ -143,7 +143,7 @@ public class CowsBullsManager : MonoBehaviour {
             GameObject currentWheel = GameObject.Find("Wheel " + i);
             userInput += currentWheel.transform.GetChild(0).GetComponent<Text>().text;
         }
-        userInput += " - " + bullsCount + " b. " + cowsCount + " c. \n";
+        userInput += " - " + bullsCount + " b " + cowsCount + " c\n";
         attemptsBoard.text += userInput;
     }
 
