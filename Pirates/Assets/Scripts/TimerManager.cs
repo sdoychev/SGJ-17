828c16d5191a0811eba369d0372bfa4d18bfd214
Stefan Doychev <sdoychev@gmail.com>
2017-01-22 04:17:18 +0200

    Improve waves

--- Pirates/Assets/Scripts/TimerManager.cs
+++ Pirates/Assets/Scripts/TimerManager.cs
@@ -26,6 +26,14 @@ public class TimerManager : MonoBehaviour {
     private float finalWaveLevel;
     private AudioSource audio;
 
+    //Move wave horizontally
+    float speed = 2f;
+    float startTime, journeyLength, distCovered, fracJourney;
+    Vector3 waveEffectPosition, randomPosition;
+    Vector3 waveEffectPosition2, randomPosition2;
+    Quaternion waveEffectRotation, randomRotation;
+    Quaternion waveEffectRotation2, randomRotation2;
+
     void Start()
     {
         initialTimerValue = timeLeft;
@@ -34,10 +42,43 @@ public class TimerManager : MonoBehaviour {
         finalWaveLevel = 518f;
         totalWavePath = Mathf.Abs(initialWaveLevel - finalWaveLevel);
         audio = GetComponent<AudioSource>();
+
+        InvokeRepeating("MoveWaveHorizontally", 0, 3);
+        InvokeRepeating("MoveWaveHorizontally2", 0, 4);
+    }
+
+    private void MoveWaveHorizontally()
+    {
+        startTime = Time.time;
+        waveEffectPosition = waveEffect.transform.position;
+        waveEffectRotation = waveEffect.transform.rotation;
+        randomPosition = new Vector3(Random.Range(-100f, 1000f), waveEffectPosition.y, waveEffectPosition.z);
+        journeyLength = Vector3.Distance(waveEffectPosition, randomPosition);
+        print(waveEffectRotation);
+        randomRotation = new Quaternion(waveEffectRotation.x, waveEffectRotation.y, Random.Range(-0.1f, 0.1f), waveEffectRotation.w);
+    }
+
+    private void MoveWaveHorizontally2()
+    {
+        startTime = Time.time;
+        waveEffectPosition2 = waveEffect.transform.GetChild(0).transform.position;
+        waveEffectRotation2 = waveEffect.transform.GetChild(0).transform.rotation;
+        randomPosition2 = new Vector3(Random.Range(-100f, 1000f), waveEffectPosition2.y, waveEffectPosition2.z);
+        journeyLength = Vector3.Distance(waveEffectPosition2, randomPosition2);
+        print(waveEffectRotation2);
+        randomRotation2 = new Quaternion(waveEffectRotation2.x, waveEffectRotation2.y, Random.Range(-0.1f, 0.1f), waveEffectRotation2.w);
     }
 
     void Update()
     {
+        distCovered = (Time.time - startTime) * speed;
+        fracJourney = distCovered / journeyLength;
+        waveEffect.transform.position = Vector3.Lerp(waveEffect.transform.position, randomPosition, fracJourney);
+        waveEffect.transform.rotation = Quaternion.Lerp(waveEffect.transform.rotation, randomRotation, fracJourney);
+
+        waveEffect.transform.GetChild(0).transform.position = Vector3.Lerp(waveEffect.transform.GetChild(0).transform.position, randomPosition2, fracJourney);
+        waveEffect.transform.GetChild(0).transform.rotation = Quaternion.Lerp(waveEffect.transform.GetChild(0).transform.rotation, randomRotation2, fracJourney);
+
         if (Input.GetKeyDown(KeyCode.P))
         {
             ReduceTimerTime();
@@ -65,6 +106,8 @@ public class TimerManager : MonoBehaviour {
     {
         waveEffect.transform.position = new Vector3(waveEffect.transform.position.x, 
             timeElapsedPercent * totalWavePath / 100 + initialWaveLevel, waveEffect.transform.position.z);
+        waveEffect.transform.GetChild(0).transform.position = new Vector3(waveEffect.transform.GetChild(0).transform.position.x,
+            timeElapsedPercent * totalWavePath / 100 + initialWaveLevel, waveEffect.transform.GetChild(0).transform.position.z);
     }
 
     public void ResetWaveLevel()
@@ -77,10 +120,13 @@ public class TimerManager : MonoBehaviour {
         timeLeft = initialTimerValue;
         timeElapsedPercent = 0;
         waveEffect.transform.position = new Vector3(waveEffect.transform.position.x, initialWaveLevel, waveEffect.transform.position.z);
+        waveEffect.transform.GetChild(0).transform.position = new Vector3(waveEffect.transform.GetChild(0).transform.position.x, initialWaveLevel, waveEffect.transform.GetChild(0).transform.position.z);
+
     }
 
     public void RemoveWaterFromScene()
     {
         waveEffect.SetActive(false);
+        waveEffect.transform.GetChild(0).transform.gameObject.SetActive(false);
     }
 }
